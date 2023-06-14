ffd3db31a9b8f2d33e4aebed9985a7e25faa3dbf
Belta <q.vinot@outlook.fr>
2023-06-14 23:52:55 +0200

    part 6

---
+++ src/pages/blog/index.js
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+import { Link, graphql } from 'gatsby'
+
+const Blog = ({data}) => {
+  const blogPostName = data.allMdx.nodes
+  console.log(blogPostName)
+  return (
+    <Layout pageTitle="My Blog Post">
+      <p>This is my blog post</p>
+      {
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <div>
+             <Link to={`/blog/${node.frontmatter.slug}`}>
+                <p>{node.frontmatter.title}</p>
+              </Link> 
+            </div>
+          </article>
+        ))
+      }
+    </Layout>
+  )
+}
+
+export const query = graphql`
+query M {
+  allMdx(sort: {
+    frontmatter: {
+      date: DESC
+    }
+  }) {
+    nodes {
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
+        slug
+      }
+      id
+      excerpt
+    }
+  }
+}
+`
+export default Blog
---
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+
+const BlogPost = () => {
+  return (
+    <Layout pageTitle="test">
+      <p>ceci est un test</p>
+    </Layout>
+  )
+}
+
+export default BlogPost
